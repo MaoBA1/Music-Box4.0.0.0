58866fa925950d9c060392bc8e3edaf7e66eb2db
Maor Bar Asher <94516860+MaoBA1@users.noreply.github.com>
2023-07-19 21:31:51 +0300

    123

---
+++ client/app.config.js
@@ -0,0 +1,11 @@
+export default {
+    expo: {
+      name: "Music-Box",
+      slug: "MusicBox",
+      icon:"./assets/icon.png",
+      updates: {
+        useClassicUpdates: true
+      }
+    },
+  };
+  
